51d3a9bd9375c66c04f8a5ee77ce4297d816349d
LeeJuYoung <rkfmgm@naver.com>
2026-04-03 02:15:15 +0900

    initial commit

---
+++ src/test/java/umc/umc10th/Umc10thApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.umc10th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc10thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
